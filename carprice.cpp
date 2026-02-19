4c4a5e9bdc901c7c6205e2dfd20e1132e12426f2
Zdjlwintl <129503525+Zdjlwintl@users.noreply.github.com>
2026-02-19 22:37:21 +0700

    Added comments to carprice.cpp

--- carprice.cpp
+++ carprice.cpp
@@ -1,4 +1,12 @@
-// 22:08
+/* Begun on 22:08
+Today is 02/19/26
+
+So this is part of the same homework where I wrote triangle.cpp, except this time we weren't actually asked to write it in
+a programming language in the first place at all LOL.
+But y'know I'll take any excuse to practice C++ hehe :3 though I won't worry about input validation much,,
+for now at the very least..
+Also for anyone interested, I'll put the pseudocode from the original question on the bottom :p
+*/
 #include <iostream>
 
 using namespace std;
@@ -33,4 +41,21 @@ int main() {
 	NewCar.NewPrice();
 	return 0;
 }
-// 22:22
+/* 22:22
+
+Pseudocode from the original question:
+
+OUTPUT “Enter purchase price”
+INPUT PurchasePrice
+CurrentValue ← PurchasePrice
+YearCount ← 1
+WHILE YearCount <= 9 AND CurrentValue > 1000
+	IF YearCount == 1 THEN
+		CurrentValue ← CurrentValue * (1 - 40 / 100)
+	ELSE
+		CurrentValue ← CurrentValue * (1 - 20 / 100)
+	ENDIF
+		OUTPUT YearCount, CurrentValue
+		YearCount ← YearCount + 1
+	ENDWHILE
+*/

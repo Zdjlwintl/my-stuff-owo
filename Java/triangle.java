8775f84f6093ad8c60b722c3bbcdf05c917bdbce
Zdjlwintl <129503525+Zdjlwintl@users.noreply.github.com>
2026-03-01 21:32:24 +0700

    Added comments to triangle.java

--- Java/triangle.java
+++ Java/triangle.java
@@ -1,3 +1,7 @@
+/* Finished on 21:26, today is 03/01/26
+so it's kinda the same as the one i made in C++, i just wanted to learn about using classes lel :p
+*/
+
 import java.util.Scanner;
 public class triangle {
 	public static class Triangle {
